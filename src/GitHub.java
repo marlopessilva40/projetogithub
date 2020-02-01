f7fe8f9c38bcfc895a2c07a6e8f06baba940826f
marlopessilva40 <marlopessilva@gmail.com>
2020-02-01 16:35:40 -0300

    versao 1.3

--- src/GitHub.java
+++ src/GitHub.java
@@ -4,6 +4,8 @@ public class GitHub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Bom dia");
+System.out.println("boa tarde");
+System.out.println("Fiz besteira");
 
 }
 }
